aa6771b0b3dc9ed0178f1967204066cb23e10ed2
Mythreyi-V <30451512+Mythreyi-V@users.noreply.github.com>
2020-10-21 15:47:03 +1000

    lstm optimisation

--- benchmark_interpretability/PPM_Stability/DatasetManager.py
+++ benchmark_interpretability/PPM_Stability/DatasetManager.py
@@ -6,6 +6,8 @@ import pandas as pd
 import numpy as np
 
 from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import MinMaxScaler
+from keras_preprocessing.sequence import pad_sequences
 
 
 class DatasetManager:
@@ -25,7 +27,9 @@ class DatasetManager:
         self.static_num_cols = dataset_confs.static_num_cols[self.dataset_name]
         
         self.sorting_cols = [self.timestamp_col, self.activity_col]
-        
+
+        self.scaler = None
+        self.encoded_cols = None
     
     def read_dataset(self):
         # read dataset
@@ -123,6 +127,9 @@ class DatasetManager:
     def get_indexes(self, data):
         return data.groupby(self.case_id_col).first().index
 
+    def get_max_case_length(self, data):
+        return data[data[self.label_col]==self.pos_label].groupby(self.case_id_col).size().max()
+
     def get_relevant_data_by_indexes(self, data, indexes):
         return data[data[self.case_id_col].isin(indexes)]
 
@@ -163,3 +170,81 @@ class DatasetManager:
             current_train_names = dt_for_splitting[self.case_id_col][train_index]
             current_test_names = dt_for_splitting[self.case_id_col][test_index]
             yield (current_train_names, current_test_names)
+
+    def encode_data_for_lstm(self, data):
+        data = data.sort_values(self.sorting_cols, ascending=True, kind='mergesort')
+        
+        num_cols = self.dynamic_num_cols + self.static_num_cols
+        cat_cols = self.dynamic_cat_cols + self.static_cat_cols
+        
+        # scale numeric cols
+        if self.scaler is None:
+            self.scaler = MinMaxScaler()
+            dt_all = pd.DataFrame(self.scaler.fit_transform(data[num_cols]), index=data.index, columns=num_cols)
+        else:
+            dt_all = pd.DataFrame(self.scaler.transform(data[num_cols]), index=data.index, columns=num_cols)
+
+        # one-hot encode categorical cols
+        dt_cat = pd.get_dummies(data[cat_cols])
+        
+        # merge
+        dt_all = pd.concat([dt_all, dt_cat], axis=1)
+        dt_all[self.case_id_col] = data[self.case_id_col]
+        dt_all[self.label_col] = data[self.label_col].apply(lambda x: 1 if x == self.pos_label else 0)
+        dt_all[self.timestamp_col] = data[self.timestamp_col]
+        
+        # add missing columns if necessary
+        if self.encoded_cols is None:
+            self.encoded_cols = dt_all.columns
+        else:
+            for col in self.encoded_cols:
+                if col not in dt_all.columns:
+                    dt_all[col] = 0
+        
+        return dt_all[self.encoded_cols]
+
+    def generate_3d_data(self, data, max_len):
+        data = data.sort_values(self.timestamp_col, ascending=True, kind="mergesort").groupby(self.case_id_col).head(max_len)
+        grouped = data.sort_values(self.timestamp_col, ascending=True, kind="mergesort").groupby(self.case_id_col)
+
+        data_dim = data.shape[1] - 3
+        n_cases = data.shape[0]
+        
+        X = np.zeros((n_cases, max_len, data_dim), dtype=np.float32)
+        y = np.zeros((n_cases, 2), dtype=np.float32)
+
+        idx = 0
+        # each prefix will be a separate instance
+        for _, group in grouped:
+            group = group.sort_values(self.timestamp_col, ascending=True, kind="mergesort")
+            label = group[self.label_col].iloc[0]
+            group = group.values
+            for i in range(1, len(group) + 1):
+                X[idx] = pad_sequences(group[np.newaxis,:i,:-3], maxlen=max_len, dtype=np.float32)
+                y[idx, label] = 1
+                idx += 1
+        return (X, y)    
+
+    def generate_3d_data_for_prefix_length(self, data, max_len, nr_events):
+        grouped = data.groupby(self.case_id_col)
+        data_dim = data.shape[1] - 3
+        n_cases = np.sum(grouped.size() >= nr_events)
+        
+        # encode only prefixes of this length
+        X = np.zeros((n_cases, max_len, data_dim), dtype=np.float32)
+        y = np.zeros((n_cases, 2), dtype=np.float32)
+        case_ids = []
+        
+        idx = 0
+        for case_id, group in grouped:
+            if len(group) < nr_events:
+                continue
+            group = group.sort_values(self.timestamp_col, ascending=True, kind="mergesort")
+            label = group[self.label_col].iloc[0]
+            group = group.values
+            X[idx] = pad_sequences(group[np.newaxis,:nr_events,:-3], maxlen=max_len, dtype=np.float32)
+            y[idx, label] = 1
+            case_ids.append(case_id)
+            idx += 1
+
+        return (X, y, case_ids)
