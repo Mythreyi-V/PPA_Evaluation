e4750a33bf42528c0f2f24757233631aa91a6cbc
Mythreyi-V <30451512+Mythreyi-V@users.noreply.github.com>
2020-11-10 16:17:27 +1000

    prepare for lyra

---
+++ benchmark_interpretability/PPM_Stability/bpic2012/lstm_single_3d/lstm_test.py
@@ -0,0 +1,9 @@
+#from google.colab import drive
+#drive.mount('/content/drive')
+import sys
+#PATH = '/content/drive/My Drive/PPM_Stability/'
+#PATH = "C:/Users/velmurug/Documents/Stability Experiments/benchmark_interpretability/PPM_Stability/"
+PATH = "~/PPM_Evaluation/Stability-Experiments/benchmark_interpretability/PPM_Stability"
+#PATH = "C:/Users/Mythreyi/Documents/GitHub/Stability-Experiments/benchmark_interpretability/PPM_Stability/"
+sys.path.append(PATH)
+
