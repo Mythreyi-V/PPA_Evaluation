d09e3f4dfc302033d6c35a20e01f80ef35f8fab6
Mythreyi-V <n9455647@qut.edu.au>
2020-12-07 12:29:45 +1000

    update all

---
+++ DatasetManager.py
@@ -0,0 +1,287 @@
+import sys
+
+import dataset_confs
+
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import MinMaxScaler
+from keras_preprocessing.sequence import pad_sequences
+from sklearn.utils import resample
+
+
+class DatasetManager:
+    
+    def __init__(self, dataset_name):
+        self.dataset_name = dataset_name
+        
+        self.case_id_col = dataset_confs.case_id_col[self.dataset_name]
+        self.activity_col = dataset_confs.activity_col[self.dataset_name]
+        self.timestamp_col = dataset_confs.timestamp_col[self.dataset_name]
+        self.label_col = dataset_confs.label_col[self.dataset_name]
+        self.pos_label = dataset_confs.pos_label[self.dataset_name]
+
+        self.dynamic_cat_cols = dataset_confs.dynamic_cat_cols[self.dataset_name]
+        self.static_cat_cols = dataset_confs.static_cat_cols[self.dataset_name]
+        self.dynamic_num_cols = dataset_confs.dynamic_num_cols[self.dataset_name]
+        self.static_num_cols = dataset_confs.static_num_cols[self.dataset_name]
+        
+        self.sorting_cols = [self.timestamp_col, self.activity_col]
+
+        self.scaler = None
+        self.encoded_cols = None
+    
+    def read_dataset(self):
+        # read dataset
+        dtypes = {col:"object" for col in self.dynamic_cat_cols+self.static_cat_cols+[self.case_id_col, self.label_col, self.timestamp_col]}
+        for col in self.dynamic_num_cols + self.static_num_cols:
+            dtypes[col] = "float"
+
+        data = pd.read_csv(dataset_confs.filename[self.dataset_name], sep=";", dtype=dtypes)
+        data[self.timestamp_col] = pd.to_datetime(data[self.timestamp_col])
+
+        return data
+
+    def balance_data(self, data):
+        y = self.get_label_numeric(data)
+        case_ids = self.get_case_ids(data)
+        
+        neg_cases = [case_ids[i] for i in range(len(case_ids)) if y[i] == 0]
+        pos_cases = [case_ids[i] for i in range(len(case_ids)) if y[i] == 1]
+
+        if len(neg_cases) > len(pos_cases):
+            neg_cases = resample(neg_cases, replace = False, n_samples = len(pos_cases))
+        elif len(neg_cases) < len(pos_cases):
+            pos_cases = resample(pos_cases, replace = False, n_samples = len(neg_cases))
+
+        bal_data = data.loc[data[self.case_id_col].isin(neg_cases)]
+        bal_data = bal_data.append(data.loc[data[self.case_id_col].isin(pos_cases)])
+        
+        return bal_data
+
+    def split_data(self, data, train_ratio, split="temporal", seed=22):  
+        # split into train and test using temporal split
+
+        grouped = data.groupby(self.case_id_col)
+        start_timestamps = grouped[self.timestamp_col].min().reset_index()
+        if split == "temporal":
+            start_timestamps = start_timestamps.sort_values(self.timestamp_col, ascending=True, kind="mergesort")
+        elif split == "random":
+            np.random.seed(seed)
+            start_timestamps = start_timestamps.reindex(np.random.permutation(start_timestamps.index))
+        train_ids = list(start_timestamps[self.case_id_col])[:int(train_ratio*len(start_timestamps))]
+        train = data[data[self.case_id_col].isin(train_ids)].sort_values(self.timestamp_col, ascending=True, kind='mergesort')
+        test = data[~data[self.case_id_col].isin(train_ids)].sort_values(self.timestamp_col, ascending=True, kind='mergesort')
+
+        return (train, test)
+    
+    def split_data_strict(self, data, train_ratio, split="temporal"):  
+        # split into train and test using temporal split and discard events that overlap the periods
+        data = data.sort_values(self.sorting_cols, ascending=True, kind='mergesort')
+        grouped = data.groupby(self.case_id_col)
+        start_timestamps = grouped[self.timestamp_col].min().reset_index()
+        start_timestamps = start_timestamps.sort_values(self.timestamp_col, ascending=True, kind='mergesort')
+        train_ids = list(start_timestamps[self.case_id_col])[:int(train_ratio*len(start_timestamps))]
+        train = data[data[self.case_id_col].isin(train_ids)].sort_values(self.sorting_cols, ascending=True, kind='mergesort')
+        test = data[~data[self.case_id_col].isin(train_ids)].sort_values(self.sorting_cols, ascending=True, kind='mergesort')
+        split_ts = test[self.timestamp_col].min()
+        train = train[train[self.timestamp_col] < split_ts]
+        return (train, test)
+    
+    def split_data_discard(self, data, train_ratio, split="temporal"):  
+        # split into train and test using temporal split and discard events that overlap the periods
+        data = data.sort_values(self.sorting_cols, ascending=True, kind='mergesort')
+        grouped = data.groupby(self.case_id_col)
+        start_timestamps = grouped[self.timestamp_col].min().reset_index()
+        start_timestamps = start_timestamps.sort_values(self.timestamp_col, ascending=True, kind='mergesort')
+        train_ids = list(start_timestamps[self.case_id_col])[:int(train_ratio*len(start_timestamps))]
+        train = data[data[self.case_id_col].isin(train_ids)].sort_values(self.sorting_cols, ascending=True, kind='mergesort')
+        test = data[~data[self.case_id_col].isin(train_ids)].sort_values(self.sorting_cols, ascending=True, kind='mergesort')
+        split_ts = test[self.timestamp_col].min()
+        overlapping_cases = train[train[self.timestamp_col] >= split_ts][self.case_id_col].unique()
+        train = train[~train[self.case_id_col].isin(overlapping_cases)]
+        return (train, test)
+    
+    
+    def split_val(self, data, val_ratio, split="random", seed=22):  
+        # split into train and test using temporal split
+        grouped = data.groupby(self.case_id_col)
+        start_timestamps = grouped[self.timestamp_col].min().reset_index()
+        if split == "temporal":
+            start_timestamps = start_timestamps.sort_values(self.timestamp_col, ascending=True, kind="mergesort")
+        elif split == "random":
+            np.random.seed(seed)
+            start_timestamps = start_timestamps.reindex(np.random.permutation(start_timestamps.index))
+        val_ids = list(start_timestamps[self.case_id_col])[-int(val_ratio*len(start_timestamps)):]
+        val = data[data[self.case_id_col].isin(val_ids)].sort_values(self.sorting_cols, ascending=True, kind="mergesort")
+        train = data[~data[self.case_id_col].isin(val_ids)].sort_values(self.sorting_cols, ascending=True, kind="mergesort")
+        return (train, val)
+
+
+    def generate_prefix_data(self, data, min_length, max_length, gap=1):
+        # generate prefix data (each possible prefix becomes a trace)
+        data['case_length'] = data.groupby(self.case_id_col)[self.activity_col].transform(len)
+
+        dt_prefixes = data[data['case_length'] >= min_length].groupby(self.case_id_col).head(min_length)
+        dt_prefixes["prefix_nr"] = 1
+        dt_prefixes["orig_case_id"] = dt_prefixes[self.case_id_col]
+        for nr_events in range(min_length+gap, max_length+1, gap):
+            tmp = data[data['case_length'] >= nr_events].groupby(self.case_id_col).head(nr_events)
+            tmp["orig_case_id"] = tmp[self.case_id_col]
+            tmp[self.case_id_col] = tmp[self.case_id_col].apply(lambda x: "%s_%s"%(x, nr_events))
+            tmp["prefix_nr"] = nr_events
+            dt_prefixes = pd.concat([dt_prefixes, tmp], axis=0)
+        
+        dt_prefixes['case_length'] = dt_prefixes['case_length'].apply(lambda x: min(max_length, x))
+        
+        return dt_prefixes
+
+
+    def get_pos_case_length_quantile(self, data, quantile=0.90):
+        return int(np.ceil(data[data[self.label_col]==self.pos_label].groupby(self.case_id_col).size().quantile(quantile)))
+
+    def get_indexes(self, data):
+        return data.groupby(self.case_id_col).first().index
+
+    def get_max_case_length(self, data):
+        return data[data[self.label_col]==self.pos_label].groupby(self.case_id_col).size().max()
+
+    def get_relevant_data_by_indexes(self, data, indexes):
+        return data[data[self.case_id_col].isin(indexes)]
+
+    def get_label(self, data):
+        return data.groupby(self.case_id_col).first()[self.label_col]
+    
+    def get_prefix_lengths(self, data):
+        return data.groupby(self.case_id_col).last()["prefix_nr"]
+    
+    def get_case_ids(self, data, nr_events=1):
+        case_ids = pd.Series(data.groupby(self.case_id_col).first().index)
+        if nr_events > 1:
+            case_ids = case_ids.apply(lambda x: "_".join(x.split("_")[:-1]))
+        return case_ids
+    
+    def get_label_numeric(self, data):
+        y = self.get_label(data) # one row per case
+        return [1 if label == self.pos_label else 0 for label in y]
+    
+    def get_class_ratio(self, data):
+        class_freqs = data[self.label_col].value_counts()
+        return class_freqs[self.pos_label] / class_freqs.sum()
+    
+    def get_stratified_split_generator(self, data, n_splits=5, shuffle=True, random_state=22):
+        grouped_firsts = data.groupby(self.case_id_col, as_index=False).first()
+        skf = StratifiedKFold(n_splits=n_splits, shuffle=shuffle, random_state=random_state)
+        
+        for train_index, test_index in skf.split(grouped_firsts, grouped_firsts[self.label_col]):
+            current_train_names = grouped_firsts[self.case_id_col][train_index]
+            train_chunk = data[data[self.case_id_col].isin(current_train_names)].sort_values(self.timestamp_col, ascending=True, kind='mergesort')
+            test_chunk = data[~data[self.case_id_col].isin(current_train_names)].sort_values(self.timestamp_col, ascending=True, kind='mergesort')
+            yield (train_chunk, test_chunk)
+            
+    def get_idx_split_generator(self, dt_for_splitting, n_splits=5, shuffle=True, random_state=22):
+        skf = StratifiedKFold(n_splits=n_splits, shuffle=shuffle, random_state=random_state)
+        
+        for train_index, test_index in skf.split(dt_for_splitting, dt_for_splitting[self.label_col]):
+            current_train_names = dt_for_splitting[self.case_id_col][train_index]
+            current_test_names = dt_for_splitting[self.case_id_col][test_index]
+            yield (current_train_names, current_test_names)
+
+    def encode_data_for_lstm(self, data):
+        data = data.sort_values(self.sorting_cols, ascending=True, kind='mergesort')
+        
+        num_cols = self.dynamic_num_cols + self.static_num_cols
+        cat_cols = self.dynamic_cat_cols + self.static_cat_cols
+        
+        # scale numeric cols
+        if self.scaler is None:
+            self.scaler = MinMaxScaler()
+            dt_all = pd.DataFrame(self.scaler.fit_transform(data[num_cols]), index=data.index, columns=num_cols)
+        else:
+            dt_all = pd.DataFrame(self.scaler.transform(data[num_cols]), index=data.index, columns=num_cols)
+        
+        # one-hot encode categorical cols
+        dt_cat = pd.get_dummies(data[cat_cols])
+        
+        
+        # merge
+        dt_all = pd.concat([dt_all, dt_cat], axis=1)
+        dt_all[self.case_id_col] = data[self.case_id_col]
+        dt_all[self.label_col] = data[self.label_col].apply(lambda x: 1 if x == self.pos_label else 0)
+        dt_all[self.timestamp_col] = data[self.timestamp_col]
+        
+        
+        # add missing columns if necessary
+        if self.encoded_cols is None:
+            self.encoded_cols = dt_all.columns
+        else:
+            for col in self.encoded_cols:
+                if col not in dt_all.columns:
+                    dt_all[col] = 0
+        
+        return dt_all[self.encoded_cols]
+
+    def generate_3d_data(self, data, max_len):
+        data = data.sort_values(self.timestamp_col, ascending=True, kind="mergesort").groupby(self.case_id_col).head(max_len)
+        grouped = data.sort_values(self.timestamp_col, ascending=True, kind="mergesort").groupby(self.case_id_col)
+
+        data_dim = data.shape[1] - 3
+        n_cases = data.shape[0]
+        
+        X = np.zeros((n_cases, max_len, data_dim), dtype=np.float32)
+        y = np.zeros((n_cases, 2), dtype=np.float32)
+        case_ids = []
+
+        idx = 0
+        # each prefix will be a separate instance
+        for case_id, group in grouped:
+            group = group.sort_values(self.timestamp_col, ascending=True, kind="mergesort")
+            label = group[self.label_col].iloc[0]
+            group = group.values
+            for i in range(1, len(group) + 1):
+                X[idx] = pad_sequences(group[np.newaxis,:i,:-3], maxlen=max_len, dtype=np.float32)
+                y[idx, label] = 1
+                case_ids.append(case_id)
+                idx += 1
+        return (X, y, case_ids)    
+
+    def generate_3d_data_for_prefix_length(self, data, max_len, nr_events):
+        grouped = data.groupby(self.case_id_col)
+        data_dim = data.shape[1] - 3
+        n_cases = np.sum(grouped.size() >= nr_events)
+        
+        # encode only prefixes of this length
+        X = np.zeros((n_cases, max_len, data_dim), dtype=np.float32)
+        y = np.zeros((n_cases, 2), dtype=np.float32)
+        case_ids = []
+        
+        idx = 0
+        for case_id, group in grouped:
+            if len(group) < nr_events:
+                continue
+            group = group.sort_values(self.timestamp_col, ascending=True, kind="mergesort")
+            label = group[self.label_col].iloc[0]
+            group = group.values
+            X[idx] = pad_sequences(group[np.newaxis,:nr_events,:-3], maxlen=max_len, dtype=np.float32)
+            y[idx, label] = 1
+            case_ids.append(case_id)
+            idx += 1
+
+        return (X, y, case_ids)
+
+    def get_lstm_encoded_cols(self, max_len):
+        #print(type(self.encoded_cols))
+        if np.any(self.encoded_cols): #self.encoded_cols == None or self.encoded_cols == []:
+            self.encode_data_for_lstm(self.read_dataset())
+        
+        features = self.encoded_cols[:-3]
+        feature_names = []
+
+        for i in range(max_len):
+            end = "_pref_"+str(i)
+            row = [feature+end for feature in features]
+            feature_names.append(row)
+
+        return(feature_names)
+        
---
+++ preprocessing/preprocess_logs_bpic2012.py
@@ -0,0 +1,109 @@
+import pandas as pd
+import numpy as np
+import os
+import sys
+
+input_data_folder = "../orig_logs"
+output_data_folder = "../labeled_logs_csv_processed_2"
+filenames = ["bpic2012.csv"]
+
+case_id_col = "Case ID"
+activity_col = "Activity"
+resource_col = "Resource"
+timestamp_col = "Complete Timestamp"
+label_col = "label"
+pos_label = "deviant"
+neg_label = "regular"
+
+relevant_offer_events = ["O_CANCELLED-COMPLETE", "O_ACCEPTED-COMPLETE", "O_DECLINED-COMPLETE"]
+
+freq_threshold = 10
+
+# features for classifier
+static_cat_cols = []
+static_num_cols = ["AMOUNT_REQ"]
+dynamic_cat_cols = [activity_col, resource_col, 'lifecycle:transition']
+dynamic_num_cols = ["timesincemidnight", "timesincelastevent", "timesincecasestart", "event_nr", "month", "weekday", "hour", "open_cases"]
+
+static_cols = static_cat_cols + static_num_cols + [case_id_col, label_col]
+dynamic_cols = dynamic_cat_cols + dynamic_num_cols + [timestamp_col]
+cat_cols = dynamic_cat_cols + static_cat_cols
+
+
+def extract_timestamp_features(group):
+    
+    group = group.sort_values(timestamp_col, ascending=False, kind='mergesort')
+    
+    tmp = group[timestamp_col] - group[timestamp_col].shift(-1)
+    tmp = tmp.fillna(pd.Timedelta(seconds = 0))
+    group["timesincelastevent"] = tmp.apply(lambda x: float(x / np.timedelta64(1, 'm'))) # m is for minutes
+
+    tmp = group[timestamp_col] - group[timestamp_col].iloc[-1]
+    tmp = tmp.fillna(pd.Timedelta(seconds = 0))
+    group["timesincecasestart"] = tmp.apply(lambda x: float(x / np.timedelta64(1, 'm'))) # m is for minutes
+
+    group = group.sort_values(timestamp_col, ascending=True, kind='mergesort')
+    group["event_nr"] = range(1, len(group) + 1)
+    
+    return group
+
+def get_open_cases(date):
+    return sum((dt_first_last_timestamps["start_time"] <= date) & (dt_first_last_timestamps["end_time"] > date))
+
+
+for filename in filenames:
+    
+    data = pd.read_csv(os.path.join(input_data_folder, filename), sep=",")
+    data[timestamp_col] = pd.to_datetime(data[timestamp_col])
+    data[resource_col] = data.sort_values(timestamp_col, ascending=True).groupby(case_id_col)[resource_col].transform(lambda grp: grp.fillna(method='ffill'))
+    data.rename(columns=lambda x: x.replace('(case) ', ''), inplace=True)
+
+    # add event duration
+    data[timestamp_col] = pd.to_datetime(data[timestamp_col])
+    data["timesincemidnight"] = data[timestamp_col].dt.hour * 60 + data[timestamp_col].dt.minute
+    data["month"] = data[timestamp_col].dt.month
+    data["weekday"] = data[timestamp_col].dt.weekday
+    data["hour"] = data[timestamp_col].dt.hour
+    
+    # add features extracted from timestamp
+    print("Extracting timestamp features...")
+    sys.stdout.flush()
+    data = data.groupby(case_id_col, as_index = False).apply(extract_timestamp_features)
+    
+    # add inter-case features
+    print("Extracting open cases...")
+    sys.stdout.flush()
+    data = data.sort_values([timestamp_col], ascending=True, kind='mergesort')
+    dt_first_last_timestamps = data.groupby(case_id_col, as_index = False)[timestamp_col].agg([min, max])
+    dt_first_last_timestamps.columns = ["start_time", "end_time"]
+    data["open_cases"] = data[timestamp_col].apply(get_open_cases)
+    
+    # assign class labels
+    last_o_events = data[data[activity_col].str.startswith("O")].sort_values(timestamp_col, ascending=True).groupby(case_id_col).last()[activity_col]
+    last_o_events = pd.DataFrame(last_o_events)
+    last_o_events.columns = ["last_o_activity"]
+    data = data.merge(last_o_events, left_on=case_id_col, right_index=True)
+    data = data[data.last_o_activity.isin(relevant_offer_events)]
+    
+    for activity in relevant_offer_events:
+        dt_labeled = data.copy()
+        dt_labeled[label_col] = neg_label
+        dt_labeled.ix[dt_labeled["last_o_activity"] == activity, label_col] = pos_label
+        
+        dt_labeled = dt_labeled[static_cols + dynamic_cols]
+        
+        # impute missing values
+        grouped = dt_labeled.sort_values(timestamp_col, ascending=True).groupby(case_id_col)
+        for col in static_cols + dynamic_cols:
+            dt_labeled[col] = grouped[col].transform(lambda grp: grp.fillna(method='ffill'))
+
+        dt_labeled[cat_cols] = dt_labeled[cat_cols].fillna('missing')
+        dt_labeled = dt_labeled.fillna(0)
+
+        # set infrequent factor levels to "other"
+        for col in cat_cols:
+            counts = dt_labeled[col].value_counts()
+            mask = dt_labeled[col].isin(counts[counts >= freq_threshold].index)
+            dt_labeled.loc[~mask, col] = "other"
+
+        dt_labeled.to_csv(os.path.join(output_data_folder, "%s_%s.csv" % (filename[:-4], activity)), sep=";", index=False)
